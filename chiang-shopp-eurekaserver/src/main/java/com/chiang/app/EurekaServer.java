235a2f7566bb6be1cf0b1bfa5b9d6799b6247a03
jiangyupeng <jiangyupeng@bjsasc.com>
2019-08-20 14:55:59 +0800

    shop project init

---
+++ chiang-shopp-eurekaserver/src/main/java/com/chiang/app/EurekaServer.java
@@ -0,0 +1,13 @@
+package com.chiang.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer.class, args);
+	}
+
+}
