05a88d549cafe370bf420425c457d264166a8f57
jiangyupeng <jiangyupeng@bjsasc.com>
2019-10-31 22:15:55 +0800

    架构打包问题修改

--- chiang-shopp-api/chiang-shopp-api-member/src/main/java/com/chiang/api/service/TestExceptionService.java
+++ chiang-shopp-api/chiang-shopp-api-member/src/main/java/com/chiang/api/service/TestExceptionService.java
@@ -1,11 +1,7 @@
 package com.chiang.api.service;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.chiang.base.ResponseBase;
 
 @RequestMapping("/testRestControllerAdivce")
 public interface TestExceptionService {
