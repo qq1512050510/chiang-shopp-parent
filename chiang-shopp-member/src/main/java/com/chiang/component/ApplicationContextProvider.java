d13817c93ff509b562c8ec29b6c6debbd840dd0e
jiangyupeng <jiangyupeng@bjsasc.com>
2020-02-02 06:46:35 +0800

    QQ SDK Jar Import

--- chiang-shopp-member/src/main/java/com/chiang/component/ApplicationContextProvider.java
+++ chiang-shopp-member/src/main/java/com/chiang/component/ApplicationContextProvider.java
@@ -1,5 +1,8 @@
 package com.chiang.component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -8,12 +11,51 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApplicationContextProvider implements ApplicationContextAware {
 
-	//Spring 上下文对象实例
+	// Spring 上下文对象实例
 	private static ApplicationContext applicationContext;
-	
+
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
+	}
+
+	/**
+	 * 通过名字获取Bean
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static Object getBean(String name) {
+		return getApplicationContext().getBean(name);
+	}
+
+	/**
+	 * 通过class获取bean
+	 * 
+	 * @param clazz
+	 * @return
+	 */
+	public static <T> T getBean(Class<T> clazz) {
+		return getApplicationContext().getBean(clazz);
+	}
+
+	/**
+	 * 通过name, 已及clazz获取指定的bean
+	 * @param name
+	 * @param clazz
+	 * @return
+	 */
+	public static <T> T getBean(String name, Class<T> clazz) {
+		return getApplicationContext().getBean(name, clazz);
+	}
+
+	public static void main(String[] args) {
+		ApplicationContextProvider.getBean(StorageTypeFactory.class);
+		Map<String, Object> map = new HashMap<String, Object>();
 	}
 
 }
