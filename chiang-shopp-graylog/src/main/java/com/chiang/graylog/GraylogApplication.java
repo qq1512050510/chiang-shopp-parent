bcf734968cfdb0c636b258e3e9f25ec2c08cdebf
jiangyupeng <jiangyupeng@bjsasc.com>
2020-03-18 17:31:37 +0800

    graylog

---
+++ chiang-shopp-graylog/src/main/java/com/chiang/graylog/GraylogApplication.java
@@ -0,0 +1,13 @@
+package com.chiang.graylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraylogApplication.class, args);
+	}
+
+}
